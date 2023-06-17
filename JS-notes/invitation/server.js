07028f8164a82b716d54e87893baa0118021ba44
monai@micous.com <monai@micous.com>
2023-06-17 18:20:44 +0800

    feat: 更新文件

--- JS-notes/invitation/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const server  = require("express")();
-const cors    = require("cors");
-
-server.use(cors({ origin : "*" }));
-
-server.use('/invitation',express.static("./web"));
-
-server.listen(130,"0.0.0.0",()=>{ console.log("http port：130") });
